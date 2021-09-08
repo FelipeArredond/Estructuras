cdd8f2ef65aed3ec2a18a1d04eb748f121e7aab8
FelipeArredond <75348223+FelipeArredond@users.noreply.github.com>
2021-09-08 11:20:14 -0500

    N=5

--- datos.py
+++ datos.py
@@ -23,16 +23,58 @@ def main():
     
     i = 0
 
-    while(i <= 0):
+    while(i <= n):
+        #Movimiento 1
         if(i == 0):
-            pos_uno = lista[(tamaño_arreglo-2)]
-            pos_dos = lista[(tamaño_arreglo-1)]
+            pos_uno = lista[tamaño_arreglo-2]
+            pos_dos = lista[tamaño_arreglo-1]
             #to
-            lista[(tamaño_arreglo-1)-1]= " "
-            lista[(tamaño_arreglo-1)-2]= " "
+            lista[tamaño_arreglo-3]= " "
+            lista[tamaño_arreglo-2]= " "
             lista[(PosA-2)] = pos_dos
-            lista[(PosA-1)] = pos_uno
-        i = i + 1
+            lista[(PosA-1)] = pos_uno  
+        #Movimiento 2
+        if(i==1):
+            pos_uno = lista[12]
+            pos_dos = lista[13]
+            #to
+            lista[12] = " "
+            lista[13] = " "
+            lista[17] = pos_uno
+            lista[18] = pos_dos
+        #Movimiento 3 
+        if(i==3):
+            pos_uno =  lista[15]
+            pos_dos = lista[16]
+            #to
+            lista[15] = " "
+            lista[16] = " "
+            lista[12] = pos_uno   
+            lista[13] = pos_dos
+        #Movimiento 4
+        if(i == 4):
+            pos_uno = lista[9]
+            pos_dos = lista[10]
+            #to
+            lista[9] = " "
+            lista[10] = " "
+            lista[15] = pos_uno
+            lista[16] = pos_dos
+
+        #Movimiento 5
+        if(i == 5):
+            pos_uno = lista[18]
+            pos_dos = lista[19]
+            #to
+            lista[18] = " "
+            lista[19] = " "
+            lista[9] = pos_uno
+            lista[10] = pos_dos    
+
+
+        #INCREMENTO
+        i  = i + 1    
+
     print(lista)        
 
 main()    
